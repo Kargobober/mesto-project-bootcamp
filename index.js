f83f93b4cb6730859bd111ccb5f6c3e85fb90606
Kargobober <kargobober@yandex.ru>
2023-08-25 19:49:33 +0300

    feat: create cards from an array

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
+/* -----Исходные данные----- */
+// Массив с картиночками
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+  {
+    name: 'Рускеала',
+    link: './images/place-ruskeala.jpg',
+  },
+  {
+    name: 'Выборг',
+    link: './images/place-vyborg.jpg',
+  },
+  {
+    name: 'Ястребиное озеро',
+    link: './images/place-yastrebinoye.jpg',
+  },
+]
+
+
 // В данном документе подразумевается, что попап и модальное окно - одно и то же
-/* Глобальные переменные */
+
+
+/* -----Глобальные переменные----- */
 const profilePopup = document.getElementById('pop-up_profile');
 const profileButtonEdit = document.querySelector('.profile__edit-button');
 const profileButtonClose = profilePopup.querySelector('.pop-up__close-button');
@@ -9,19 +53,21 @@ const profileForm = document.forms["form-profile"];
 const profileUsername = document.querySelector('.profile__username');
 const profileAbout = document.querySelector('.profile__user-about');
 const profileButtonSave = profileForm.querySelector('.form__submit-button');
+// У объекта template в его свойстве content сразу находим элемент списка (элемент вёрстки)
+const templateCard = document.getElementById('template-element').content.querySelector('.element');
+const cards = document.querySelector('.elements__list');
 
 
-
-/* Функции */
-function openPopup (popupElement) {
+/* -----Функции----- */
+function openPopup(popupElement) {
   popupElement.classList.add('pop-up_opened');
 }
 
-function closePopup (popupElement) {
+function closePopup(popupElement) {
   popupElement.classList.remove('pop-up_opened');
 }
 
-function saveProfile (evt) {
+function saveProfile(evt) {
   evt.preventDefault();
   const usernameModal = profilePopup.querySelector('input[name="name"]');
   const userAboutModal = profilePopup.querySelector('input[name="about"]');
@@ -30,10 +76,30 @@ function saveProfile (evt) {
   closePopup(profilePopup);
 }
 
+function createCard(name, imgLink) {
+  const newCard = templateCard.cloneNode(true);
+  const cardName = newCard.querySelector('.element__heading');
+  const cardImage = newCard.querySelector('.element__image');
+  cardName.textContent = name;
+  cardImage.setAttribute('src', imgLink);
+  cardImage.setAttribute('alt', name);
+  return newCard;
+}
 
-/* Обработчики событий */
+function insertCard(item) {
+  cards.append(item);
+}
+
+
+/* -----Логика----- */
+initialCards.forEach(item => {
+  const newCard = createCard(item.name, item.link);
+  insertCard(newCard);
+});
+
+/* -----Обработчики событий----- */
 // Нажатие на редактирование профиля
-profileButtonEdit.addEventListener('click', function (evt) {
+profileButtonEdit.addEventListener('click', function(evt) {
   const usernameModal = profilePopup.querySelector('input[name="name"]');
   const userAboutModal = profilePopup.querySelector('input[name="about"]');
   usernameModal.value = profileUsername.textContent;
@@ -42,7 +108,7 @@ profileButtonEdit.addEventListener('click', function (evt) {
 });
 
 // Нажатие на крестик в попапе профиля
-profileButtonClose.addEventListener('click', function (evt) {
+profileButtonClose.addEventListener('click', function(evt) {
   closePopup(profilePopup);
 });
 
